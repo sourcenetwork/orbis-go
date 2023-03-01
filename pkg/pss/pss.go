e927e17b9e14f76a3b820bfcb70c4f12b71ed068
Roy Lee <roylee17@gmail.com>
2023-02-28 22:43:20 -0800

    pss, pre: Move the Group out of PSS interface
    1. Make the PSS focus on the DKG (and PSS)
    2. Make PRE depending on Group and PSS (for now).
    3. Refactor the variable and rework the commentary
    
    Note:
    
       The PRE can be further decoupled from the PSS, and focus on the math:
    
       - number of participants
       - public polynomial
       - shares collected
       - threshold

--- pkg/pss/pss.go
+++ pkg/pss/pss.go
@@ -3,11 +3,10 @@ package pss
 import (
 	"context"
 
+	"github.com/pact-foundation/pact-go/types"
 	"github.com/samber/do"
-	"go.dedis.ch/kyber/v3/suites"
 
 	"github.com/sourcenetwork/orbis-go/pkg/crypto"
-	"github.com/sourcenetwork/orbis-go/pkg/pss/types"
 	"github.com/sourcenetwork/orbis-go/pkg/transport"
 )
 
@@ -16,20 +15,20 @@ type Message interface{}
 type Service interface {
 	// Name of the PSS Algorithm
 	Name() string
-	// Cryptographic suite
-	Suite() suites.Suite
 
 	// Start the service
 	Start()
+
 	// Shutdown the service
 	Shutdown() error
-	// Process incoming messages relating to the
-	// operations or maintenence of the PSS/DKG
-	// algorithm
+
+	// Process incoming messages relating to the operations or maintenence
+	// of the PSS/DKG algorithm
 	ProcessMessage(context.Context, Message)
 
 	// Aggregate public key of the PSS/DKG
 	PublicKey() crypto.PublicKey
+
 	// Public polynomial
 	PublicPoly() crypto.PubPoly
 
