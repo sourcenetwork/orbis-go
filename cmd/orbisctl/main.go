42921045565d7fb684699c4c893be60df885a061
Roy Lee <roylee17@gmail.com>
2023-03-26 22:50:48 -0700

    cmd: import orbisctl

---
+++ cmd/orbisctl/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"time"
+
+	ringv1alpha1 "github.com/sourcenetwork/orbis-go/gen/proto/ring/v1alpha1"
+	secretv1alpha1 "github.com/sourcenetwork/orbis-go/gen/proto/secret/v1alpha1"
+
+	"github.com/NathanBaulch/protoc-gen-cobra/client"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+
+	rootCmd := &cobra.Command{
+		Use:          "orbisctl",
+		Long:         "orbisctl controls the Orbis server",
+		SilenceUsage: true,
+	}
+
+	opts := []client.Option{
+		client.WithTimeout(1 * time.Second),
+	}
+
+	// Setup client commands for the Orbis client.
+	rootCmd.AddCommand(
+		ringv1alpha1.RingServiceClientCommand(opts...),
+		secretv1alpha1.SecretServiceClientCommand(opts...),
+	)
+
+	rootCmd.Execute()
+}
