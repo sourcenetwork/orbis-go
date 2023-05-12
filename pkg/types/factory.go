b9a81f45ae781c559187846644431bfbd4e6386d
John-Alan Simmons <simmons.johnalan@gmail.com>
2023-05-12 13:59:21 -0400

    added type factory

---
+++ pkg/types/factory.go
@@ -0,0 +1,12 @@
+package types
+
+import (
+	"github.com/samber/do"
+	"github.com/sourcenetwork/orbis-go/pkg/db"
+)
+
+type Factory[T any] interface {
+	New(*do.Injector, []*db.RepoKey) (T, error)
+	Name() string
+	Repos() []string
+}
