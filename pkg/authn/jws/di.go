3ec7f456fdab1e9395787c51fc3eff63e6dd1926
John-Alan Simmons <simmons.johnalan@gmail.com>
2023-08-03 19:31:16 -0400

    Added DI for JWS

---
+++ pkg/authn/jws/di.go
@@ -0,0 +1,39 @@
+package jws
+
+import (
+	"fmt"
+
+	"github.com/samber/do"
+	"github.com/sourcenetwork/orbis-go/pkg/authn"
+	"github.com/sourcenetwork/orbis-go/pkg/db"
+	"github.com/sourcenetwork/orbis-go/pkg/types"
+)
+
+var (
+	_                 types.Factory[authn.CredentialService] = (*selfSignedFactory)(nil)
+	SelfSignedFactory                                        = selfSignedFactory{}
+)
+
+type selfSignedFactory struct{}
+
+func (selfSignedFactory) New(inj *do.Injector, rkeys []db.RepoKey) (authn.CredentialService, error) {
+	resolver, err := do.Invoke[authn.KeyResolver](inj)
+	if err != nil {
+		return nil, fmt.Errorf("invoke key resolver: %w", err)
+	}
+	metadataFn, err := do.Invoke[authn.RequestMetadataParser](inj)
+	if err != nil {
+		return nil, fmt.Errorf("invoke metadata parser: %w", err)
+	}
+	return NewSelfSignedCredentialService(resolver, metadataFn), nil
+}
+
+func (selfSignedFactory) Name() string {
+	return "jws-did"
+}
+
+// Repos returns empty string to indicate no dependent repos needed
+// which means an empty array will be passed to `New` for `rkeys`
+func (selfSignedFactory) Repos() []string {
+	return []string{}
+}
