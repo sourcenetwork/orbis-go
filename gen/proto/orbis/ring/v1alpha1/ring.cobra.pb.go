b9a3306ef55c7177d32181be10953f08891a8867
John-Alan Simmons <simmons.johnalan@gmail.com>
2023-10-30 18:20:53 -0400

    Generated new secret types

--- gen/proto/orbis/ring/v1alpha1/ring.cobra.pb.go
+++ gen/proto/orbis/ring/v1alpha1/ring.cobra.pb.go
@@ -435,6 +435,8 @@ func _RingServiceStoreSecretCommand(cfg *client.Config) *cobra.Command {
 	flag.WithPostSetHook(cmd.PersistentFlags(), cfg.FlagNamer("Secret EncCmt"), func() { req.Secret = _Secret })
 	flag.BytesBase64SliceVar(cmd.PersistentFlags(), &_Secret.EncScrt, cfg.FlagNamer("Secret EncScrt"), "")
 	flag.WithPostSetHook(cmd.PersistentFlags(), cfg.FlagNamer("Secret EncScrt"), func() { req.Secret = _Secret })
+	flag.BytesBase64Var(cmd.PersistentFlags(), &_Secret.AuthzCtx, cfg.FlagNamer("Secret AuthzCtx"), "")
+	flag.WithPostSetHook(cmd.PersistentFlags(), cfg.FlagNamer("Secret AuthzCtx"), func() { req.Secret = _Secret })
 
 	return cmd
 }
